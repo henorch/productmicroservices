1961bc7a8b9a8a62a6121fae1ba30d77ed33b544
henorch <henorch01@gmail.com>
2024-04-07 11:51:00 +0100

    everything commited

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import typeDefs from './src/productTypeDef.js'
 import resolvers from './src/productsResolver.js'
+import { DatabaseConnection } from './utils/dbConnection.js'
+import { mongoose } from 'mongoose'
+
+mongoose.connect("mongodb+srv://henorch01:fGwz0p9jJJbN5hUx@damart.3escttb.mongodb.net/?retryWrites=true&w=majority&appName=Damart").then(() => {
+    console.log('database succefully connected');
+})
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    introspection: true,
+    cors: {
+        origin:'*',
+        credentials: true
+    }
 })
 
 const { url } = await startStandaloneServer(server, {
-    listen:{port: 3001 }
+    listen:{port: 4001 }
 })
 console.log(`server running on ${url}`);
